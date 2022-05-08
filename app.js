ce52f88e0c2d8bbf43aae211c8cba89b26c06784
Md Shahab Uddin <71219156+shb-services@users.noreply.github.com>
2022-05-09 01:10:32 +0600

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ setInterval(() => {
     if (h > 12) {
         h = h - 12;
     } 
-    // when time is 12 AM then it will show 0 for 24H system. Here is convert it into 12 by below code
+    // when time is 12 AM then it will show 0 for 24H system. 
+    //Now it will change 00 to 12 by below code
     h == 0 ? h = 12 : '' ;
     
     // add zero before single digit number
